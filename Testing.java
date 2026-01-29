7fa242faaea1eb5de3e66847964391746a055031
Chloe <cflirio@cpp.edu>
2026-01-29 10:06:21 -0800

    Add debug prints to Testing.java for debugging lesson

--- Testing.java
+++ Testing.java
@@ -1,8 +1,13 @@
-// make a simple program that has one bug in it.
+// Program to test debugging - intentional bug for learning
 public class Testing {
     public static void main(String[] args) {
         int a = 5;
         int b = 0;
+        System.out.println("Starting calculation...");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        // This line has the bug (division by zero)
         int c = a / b;
         System.out.println("Result: " + c);
     }
