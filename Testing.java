1c343d746d9703f11c5ad2953ef1975b3108f4e4
Chloe <cflirio@cpp.edu>
2026-01-29 20:36:13 -0800

    "Debugging fourth commit"

--- Testing.java
+++ Testing.java
@@ -1,8 +1,8 @@
 // Program to test debugging - intentional bug for learning
 public class Testing {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
+        int a = 10;
+        int b = 0;
         int c = a / b;
         System.out.println("Result: " + c);
     }
